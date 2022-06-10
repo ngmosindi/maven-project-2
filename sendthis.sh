b1e00b6cb2eeb54adba42482d35d2269039e320c
ngoziokon <ngmosindi@yahoo.com>
2022-06-10 03:14:58 +0100

    send this

---
+++ sendthis.sh
@@ -0,0 +1,10 @@
+############install Python 3  #######################
+
+
+
+#Install python prereqs
+
+apt install -y build-essential zlib1g-dev libbz2-dev libreadline-dev libsqlite3-dev llvm libncurses5-dev libncursesw5-dev xz-utils tk-dev
+
+
+
